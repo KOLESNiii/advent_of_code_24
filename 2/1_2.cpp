faeb4868a68a99dc00a85355f0bb4cbdc51bf8fd
Tim Kolesnichenko <timofey.kolesnichenko24@imperial.ac.uk>
2024-12-02 05:17:49 +0000

    problem 1

---
+++ 2/1_2.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+bool isSafe(vector<ll> nums) {
+    ll a;
+    a = nums[0];
+    int increasing = 0;
+    bool invalid = false;
+    for (ll b : vector<ll>(nums.begin()+1, nums.end())) {
+        if (abs(a-b) < 1 || abs(a-b) > 3) {
+            invalid = true;
+            break;
+        } else {
+            if (increasing == 0) {
+                if (a-b > 0) {
+                    increasing = -1;
+                } else {
+                    increasing = 1;
+                }
+            } else if (increasing == 1) {
+                if (a-b > 0) {
+                    invalid = true;
+                    break;
+                }
+            } else if (increasing == -1) {
+                if (b-a > 0) {
+                    invalid = true;
+                    break;
+                }
+            }
+        }
+        a = b;
+    }
+}
+
+int main() {
+    std::ifstream inputFile("input.txt");
+    
+
+    string line;
+    ll s = 0;
+    while(getline(inputFile, line)) {
+        istringstream report(line);
+        ll a, b;
+        report >> a;
+        int increasing = 0;
+        bool invalid = false;
+        while (report >> b) {
+            if (abs(a-b) < 1 || abs(a-b) > 3) {
+                invalid = true;
+                break;
+            } else {
+                if (increasing == 0) {
+                    if (a-b > 0) {
+                        increasing = -1;
+                    } else {
+                        increasing = 1;
+                    }
+                } else if (increasing == 1) {
+                    if (a-b > 0) {
+                        invalid = true;
+                        break;
+                    }
+                } else if (increasing == -1) {
+                    if (b-a > 0) {
+                        invalid = true;
+                        break;
+                    }
+                }
+            }
+            a = b;
+        }
+        if (! invalid) {
+            s ++;
+        }
+    }
+    inputFile.close();
+    cout << s;
+    return 0;
+}
