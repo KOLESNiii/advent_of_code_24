b98591f6222ef7784bde1c8b0dc91a576574a445
Tim Kolesnichenko <timofey.kolesnichenko24@imperial.ac.uk>
2024-12-03 05:14:10 +0000

    finished a

---
+++ 3/1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    std::ifstream inputFile("input.txt");
+    string line;
+    ll s = 0;
+    regex mulRegex(R"(mul\((\d+),(\d+)\))");
+    smatch match;
+    while(getline(inputFile, line)) {
+        auto begin = sregex_iterator(line.begin(), line.end(), mulRegex);
+        auto end = sregex_iterator();
+        for (auto it = begin; it != end; ++it) {
+            ll a = stoll((*it)[1].str());
+            ll b = stoll((*it)[2].str());
+            s += a * b;
+        }
+
+    }
+    inputFile.close();
+    cout << s;
+    return 0;
+}
