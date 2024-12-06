c218384778302dd8d4a615aa0f408a84e3d8d739
Tim Kolesnichenko <timofey.kolesnichenko24@imperial.ac.uk>
2024-12-06 19:00:02 +0000

    Massively optimised

--- 6/2.cpp
+++ 6/2.cpp
@@ -2,32 +2,43 @@
 using namespace std;
 typedef long long ll;
 
-vector<ll> rotateRight(vector<ll> dir) {
+struct VectorHash {
+    size_t operator()(const array<ll, 2>& vec) const {
+        std::size_t seed = 0;
+        for (ll num : vec) {
+            seed ^= hash<ll>()(num) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        }
+        return seed;
+    }
+    size_t operator()(const array<ll, 4>& vec) const {
+        std::size_t seed = 0;
+        for (ll num : vec) {
+            seed ^= hash<ll>()(num) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        }
+        return seed;
+    }
+};
+
+array<ll, 2> rotateRight(array<ll, 2> dir) {
     return {-dir[1], dir[0]};
 }
 
 int main() {
     ifstream inputFile("input.txt");
-    ifstream ruleFile("rules.txt");
     string line;
-    set<vector<ll>> obstacles;
-    set<vector<ll>> dots;
-    vector<ll> curPos;
+    unordered_set<array<ll, 2>, VectorHash> obstacles;
+    array<ll, 2> curPos;
     ll y = 0;
     ll maxX, maxY;
     while (getline(inputFile, line)) {
         ll x = 0;
-        vector<ll> temp;
         istringstream iss(line);
         char tk;
         while (iss >> tk) {
             if (tk == '^') {
-                cout << x << ", " << y << endl;
                 curPos = {x, y};
             } else if (tk == '#') {
                 obstacles.insert({x, y});
-            } else if (tk == '.') {
-                dots.insert({x, y});
             }
             ++x;
         }
@@ -36,10 +47,10 @@ int main() {
     }
     maxY = y;
     ll s = 0;
-    vector<ll> curDir = {0, -1};
-    vector<ll> startDir = curDir;
-    vector<ll> startPos = curPos;
-    set<vector<ll>> visitedFirst;
+    array<ll, 2> curDir = {0, -1};
+    array<ll, 2> startDir = curDir;
+    array<ll, 2> startPos = curPos;
+    unordered_set<array<ll, 2>, VectorHash> visitedFirst;
     visitedFirst.insert(startPos);
     while (curPos[0] < maxX && curPos[1] < maxY && curPos[0] >= 0 && curPos[1] >= 0) {
         ll curX = curPos[0];
@@ -48,14 +59,14 @@ int main() {
         ll dY = curDir[1];
         if (obstacles.count({curX + dX, curY + dY}) == 0) {
             curPos = {curX + dX, curY + dY};
-            if (curPos[0] <= maxX && curPos[1] <= maxY && curPos[0] >= 0 && curPos[1] >= 0 && dots.count({curX, curY}) != 0) {
+            if (curPos[0] <= maxX && curPos[1] <= maxY && curPos[0] >= 0 && curPos[1] >= 0) {
                 visitedFirst.insert({curX, curY});
             }
         } else {
             curDir = rotateRight(curDir);
         }
     }
-    set<vector<ll>> visited;
+    unordered_set<array<ll, 4>, VectorHash> visited;
     for (auto it = visitedFirst.begin(); it != visitedFirst.end(); ++it) {
         ll obstacleX = (*it)[0];
         ll obstacleY = (*it)[1];
