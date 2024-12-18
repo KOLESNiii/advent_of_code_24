279417d10ee9fdc32bd1e3d212e691c19904f570
Tim Kolesnichenko <timofey.kolesnichenko24@imperial.ac.uk>
2024-12-18 14:49:04 +0000

    Much better

--- 18/1_2.cpp
+++ 18/1_2.cpp
@@ -2,110 +2,67 @@
 using namespace std;
 typedef long long ll;
 
-ll XMAX = 70;
-ll YMAX = 70;
-ll NUMBYTES = 1024;
+constexpr ll XMAX = 70;
+constexpr ll YMAX = 70;
+constexpr ll NUMBYTES = 1024;
 
-struct ArrayHash {
-    size_t operator()(const array<ll, 2>& arr) const {
-        size_t seed = 0;
-        for (ll val : arr) {
-            seed ^= hash<ll>{}(val) + 0x9e3778b9 + (seed << 6) + (seed >> 2); 
+vector<array<ll, 2>> DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+ll getMinScore(vector<vector<bool>>& blocked) {
+    vector<vector<ll>> dist(XMAX + 1, vector<ll>(YMAX + 1, INT64_MAX));
+    priority_queue<array<ll, 3>, vector<array<ll, 3>>, greater<>> pq;
+    pq.push({0, 0, 0});
+    dist[0][0] = 0;
+
+    while (!pq.empty()) {
+        auto [steps, x, y] = pq.top();
+        pq.pop();
+
+        if (x == XMAX && y == YMAX) {
+            return steps;
         }
-        return seed;
-    }
-    size_t operator()(const array<ll, 3>& arr) const {
-        size_t seed = 0;
-        for (ll val : arr) {
-            seed ^= hash<ll>{}(val) + 0x9e3778b9 + (seed << 6) + (seed >> 2); 
-        }
-        return seed;
-    }
-};
 
-vector<array<ll, 2>> DIRS = {{1,0}, {0, 1}, {-1, 0}, {0, -1}};
-
-pair<ll, unordered_map<array<ll, 2>, ll, ArrayHash>> getMinScore(const unordered_set<array<ll, 2>, ArrayHash>& blocked) {
-    unordered_map<array<ll, 2>, ll, ArrayHash> visited;
-    priority_queue<array<ll, 3>, vector<array<ll, 3>>, greater<>> q;
-    q.push({0, 0, 0});
-    visited[{0, 0}] = 0;
-    ll minVal = INT64_MAX;
-    while (!q.empty()) {
-        auto [x, y, s] = q.top();
-        q.pop();
         for (auto [dx, dy] : DIRS) {
-            ll nx = x + dx;
-            ll ny = y + dy;
-            if (nx > XMAX || ny > YMAX || nx < 0 || ny < 0) {
-                continue;
-            }
-            if (nx == XMAX && ny == YMAX) {
-                return {s+1, visited};
-            }
-            if (!blocked.count({nx, ny})) {
-                if (!visited.count({nx, ny}) || visited[{nx, ny}] > s+1) {
-                    visited[{nx, ny}] = s+1;
-                    minVal = min(s+1, minVal);
-                    q.push({nx, ny, s+1});
+            ll nx = x + dx, ny = y + dy;
+            if (nx >= 0 && nx <= XMAX && ny >= 0 && ny <= YMAX && !blocked[nx][ny]) {
+                if (dist[nx][ny] > steps + 1) {
+                    dist[nx][ny] = steps + 1;
+                    pq.push({steps + 1, nx, ny});
                 }
             }
         }
     }
-    return {-1, visited};
-}
-
-unordered_set<array<ll, 2>, ArrayHash> getRoute(unordered_map<array<ll, 3>, ll, ArrayHash>& visited, ll maxVal) {
-    queue<array<ll, 3>> q;
-    q.push({XMAX, YMAX, maxVal});
-    unordered_set<array<ll, 2>, ArrayHash> route;
-    while (!q.empty()) {
-        auto [x, y, s] = q.front();
-        q.pop();
-        route.insert({x, y});
-        for (auto [dx, dy] : DIRS) {
-            ll nx = x + dx;
-            ll ny = y + dy;
-            if (visited.count({nx, ny}) && visited[{nx, ny}] == s - 1) {
-                q.push({nx, ny, s - 1});
-            }
-        }
-    } 
-    return route;
+    return -1;
 }
 
 int main() {
     ifstream inputFile("input.txt");
-    unordered_set<array<ll, 2>, ArrayHash> blocked;
-    queue<array<ll, 2>> q;
-    regex pattern(R"((\d+),(\d+))");
-    smatch match;
+    vector<vector<bool>> blocked(XMAX + 1, vector<bool>(YMAX + 1, false));
+
+    queue<array<ll, 2>> additionalBlocks;
     string line;
     ll countBytes = 0;
     while (getline(inputFile, line)) {
-        if (regex_search(line, match, pattern)) {
-            ll x = stoll(match[1]);
-            ll y = stoll(match[2]);
-            if (countBytes < NUMBYTES) {
-                blocked.insert({x, y});
-                countBytes ++;
-            } else {
-                q.push({x, y});
-            }
+        ll x, y;
+        sscanf(line.c_str(), "%lld,%lld", &x, &y);
+        if (countBytes < NUMBYTES) {
+            blocked[x][y] = true;
+            countBytes++;
+        } else {
+            additionalBlocks.push({x, y});
         }
     }
-    auto [s, visited] = getMinScore(blocked);
-    cout << "Part 1: " << s << endl;
-    while (!q.empty()) {
-        array<ll, 2> nb = q.front();
-        q.pop();
-        blocked.insert(nb);
-        if (visited.count(nb)) {
-            auto [s, visited] = getMinScore(blocked);
-            if (s == -1) {
-                cout << "Part 2: " << nb[0] << "," << nb[1] << endl;
-                break;
-            }
+
+    ll minSteps = getMinScore(blocked);
+    cout << "Part 1: " << minSteps << endl;
+    while (!additionalBlocks.empty()) {
+        auto [x, y] = additionalBlocks.front();
+        additionalBlocks.pop();
+
+        blocked[x][y] = true;
+        if (getMinScore(blocked) == -1) {
+            cout << "Part 2: " << x << "," << y << endl;
+            break;
         }
     }
     return 0;
