15c013b3eb7c4a9322da3413444075c7229c1fb2
Tim Kolesnichenko <timofey.kolesnichenko24@imperial.ac.uk>
2024-12-20 12:21:36 +0000

    Cleaned up

--- 20/1_2.cpp
+++ 20/1_2.cpp
@@ -12,13 +12,6 @@ struct ArrayHash {
         }
         return seed;
     }
-    size_t operator()(const array<ll, 5>& arr) const {
-        size_t seed = 0;
-        for (ll val : arr) {
-            seed ^= hash<ll>{}(val) + 0x9e3778b9 + (seed << 6) + (seed >> 2); 
-        }
-        return seed;
-    }
     size_t operator()(const array<ll, 3>& arr) const {
         size_t seed = 0;
         for (ll val : arr) {
@@ -37,7 +30,7 @@ struct ArrayHash {
 
 vector<array<ll, 2>> DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-tuple<ll, unordered_set<array<ll, 2>, ArrayHash>, vector<vector<ll>>> getMinScore(vector<vector<bool>>& blocked, array<ll, 2>& start, array<ll, 2>& end) {
+tuple<unordered_set<array<ll, 2>, ArrayHash>, vector<vector<ll>>> getDistanceMatrix(vector<vector<bool>>& blocked, array<ll, 2>& start, array<ll, 2>& end) {
     vector<vector<ll>> dist(XMAX + 1, vector<ll>(YMAX + 1, INT64_MAX));
     priority_queue<array<ll, 3>, vector<array<ll, 3>>, greater<>> pq;
     unordered_set<array<ll, 2>, ArrayHash> visited;
@@ -50,7 +43,7 @@ tuple<ll, unordered_set<array<ll, 2>, ArrayHash>, vector<vector<ll>>> getMinScor
         visited.insert({x, y});
 
         if (end[0] == x && end[1] == y) {
-            return {steps, visited, dist};
+            return {visited, dist};
         }
 
         for (auto [dx, dy] : DIRS) {
@@ -63,24 +56,17 @@ tuple<ll, unordered_set<array<ll, 2>, ArrayHash>, vector<vector<ll>>> getMinScor
             }
         }
     }
-    return {-1, visited, dist};
+    return {visited, dist};
 }
 
-unordered_set<array<ll, 4>, ArrayHash> getAllCheats(unordered_set<array<ll, 2>, ArrayHash>& possible, vector<vector<ll>>& dist) {
+unordered_set<array<ll, 4>, ArrayHash> getAllCheats(unordered_set<array<ll, 2>, ArrayHash>& possible, vector<vector<bool>>& blocked, vector<vector<ll>>& dist, ll maxTime) {
     unordered_set<array<ll, 4>, ArrayHash> s;
 
     for (auto [x, y] : possible) {
         queue<array<ll, 3>> q;
         unordered_set<array<ll, 3>, ArrayHash> visited;
-
-        for (auto [dx, dy] : DIRS) {
-            ll nx = x + dx, ny = y + dy;
-            if (nx >= 0 && nx <= XMAX && ny >= 0 && ny <= YMAX) {
-                q.push({1, nx, ny});
-                visited.insert({1, nx, ny});
-            }
-        }
-
+        q.push({0, x, y});
+        visited.insert({0, x, y});
         while (!q.empty()) {
             auto [n, tx, ty] = q.front();
             q.pop();
@@ -89,12 +75,12 @@ unordered_set<array<ll, 4>, ArrayHash> getAllCheats(unordered_set<array<ll, 2>, 
                 ll nx = tx + dx, ny = ty + dy;
 
                 if (nx < 0 || nx > XMAX || ny < 0 || ny > YMAX) continue;
-                if (n < 20 && !visited.count({n+1, nx, ny})) {
+                if (n < maxTime && !visited.count({n+1, nx, ny})) {
                     visited.insert({n+1, nx, ny});
                     q.push({n+1, nx, ny});
                     if (possible.count({nx, ny})) {
                         ll timeSaved = dist[nx][ny] - dist[x][y] - (n+1);
-                        if (timeSaved >= 1000) {
+                        if (timeSaved >= 100) {
                             s.insert({x, y, nx, ny});
                         }
                     }
@@ -117,11 +103,8 @@ int main() {
         vector<bool> temp;
         for (char c : line) {
             temp.push_back(c == '#');
-            if (c == 'S') {
-                start = {x, y};
-            } else if (c == 'E') {
-                end = {x, y};
-            }
+            if (c == 'S') start = {x, y};
+            else if (c == 'E') end = {x, y};
             x ++;
         }
         blocked.push_back(temp);
@@ -129,10 +112,11 @@ int main() {
         y ++;
     }
     YMAX = y;
-    auto [minSteps, visited, dist] = getMinScore(blocked, start, end);
-    cout << minSteps << endl;
-    unordered_set<array<ll, 4>, ArrayHash> all = getAllCheats(visited, dist);
-    ll s = all.size();
-    cout << "Part 2: " << s << endl;
+    auto [visited, dist] = getDistanceMatrix(blocked, start, end);
+    array<ll, 2> limits = {2, 20};
+    for (int i = 0; i < 2; ++i) {
+        unordered_set<array<ll, 4>, ArrayHash> all = getAllCheats(visited, blocked, dist, limits[i]);
+        cout << "Part " << i+1 << ": " << all.size() << endl;
+    }
     return 0;
 }
